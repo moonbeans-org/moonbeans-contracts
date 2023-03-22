6fd5b6be6bf90896022313bb4ec24a1c96f5b966
Smol Bean <smols@moonbeans.io>
2023-03-21 22:41:21 -0500

    redeployed nova

--- scripts/constants.js
+++ scripts/constants.js
@@ -14,9 +14,13 @@ export const addressesArbitrumNova = {
     WETH: '0x722E8BdD2ce80A4422E880164f2079488e115365',
     BeanieDistributor: '0x25878392F62D65B0FE15Ec49C026D2B7aBEdddCF',
     FeeProcessor: '0x24c5CB884BF3AC3F6ceB0d41F90987B6fe0Dd4A7',
-    BeanieMarket: '0x2f56AcA0F55062901735245a87C7c1af9552fC06',
-    FungibleBeanieMarketV1: '0x3A6dffd314818A37FA67c23465cf7992DdF229d6'
+    BeanieMarket: '0x56892CBf4BFf9FCDE4E65076b135440F315a91f2',
+    FungibleBeanieMarketV1: '0x0d45F008283943E7F91Cf8af0e9e7Ea85d1ab01d'
 };
+
+export const addressesArbitrumOne = {
+    WETH: '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1'
+}
 
 export const addressesMoonbase = {
     StorefrontOwnership: "0x6e0fa1dC8E3e6510aeBF14fCa3d83C77a9780ecB"
